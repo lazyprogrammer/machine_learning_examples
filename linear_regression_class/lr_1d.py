cd98c551c7ce3c4c822a8dfd212791ce2259c356
Sublime Balloon <sublime.balloon@gmail.com>
2015-08-16 16:57:13 -0400

    add r-squared

--- linear_regression_class/lr_1d.py
+++ linear_regression_class/lr_1d.py
@@ -35,3 +35,9 @@ Yhat = a*X + b
 plt.scatter(X, Y)
 plt.plot(X, Yhat)
 plt.show()
+
+# determine how good the model is by computing the r-squared
+d1 = Y - Yhat
+d2 = Y - Y.mean()
+r2 = 1 - d1.dot(d1) / d2.dot(d2)
+print "the r-squared is:", r2
