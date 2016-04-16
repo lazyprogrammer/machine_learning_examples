701e19eaf5fae7f4dfb01799c1a373a095f8283f
Mac User <sublime.balloon@gmail.com>
2016-04-16 01:44:34 -0400

    add unsupervised class

---
+++ unsupervised_class/gmm.py
@@ -0,0 +1,84 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from scipy.stats import multivariate_normal
+
+
+def gmm(X, K, max_iter=20):
+    N, D = X.shape
+    M = np.zeros((K, D))
+    R = np.zeros((N, K))
+    C = np.zeros((K, D, D))
+    pi = np.ones(K) / K # uniform
+
+    # initialize M to random, initialize C to spherical with variance 1
+    for k in xrange(K):
+        M[k] = X[np.random.choice(N)]
+        C[k] = np.diag(np.ones(D))
+
+    costs = np.zeros(max_iter)
+    weighted_pdfs = np.zeros((N, K)) # we'll use these to store the PDF value of sample n and Gaussian k
+    for i in xrange(max_iter):
+        # step 1: determine assignments / resposibilities
+        for k in xrange(K):
+            for n in xrange(N):
+                weighted_pdfs[n,k] = pi[k]*multivariate_normal.pdf(X[n], M[k], C[k])
+
+        for k in xrange(K):
+            for n in xrange(N):
+                R[n,k] = weighted_pdfs[n,k] / weighted_pdfs[n,:].sum()
+
+        # step 2: recalculate params
+        for k in xrange(K):
+            Nk = R[:,k].sum()
+            pi[k] = Nk / N
+            M[k] = R[:,k].dot(X) / Nk
+            X_minus_mu = X - M[k]
+            C[k] = np.sum(R[n,k]*np.outer(X[n] - M[k], X[n] - M[k]) for n in xrange(N)) / Nk + np.diag(np.ones(D)*0.001)
+
+
+        costs[i] = np.log(weighted_pdfs.sum(axis=1)).sum()
+        if i > 0:
+            if np.abs(costs[i] - costs[i-1]) < 0.1:
+                break
+
+    plt.plot(costs)
+    plt.title("Costs")
+    plt.show()
+
+    random_colors = np.random.random((K, 3))
+    colors = R.dot(random_colors)
+    plt.scatter(X[:,0], X[:,1], c=colors)
+    plt.show()
+
+    print "pi:", pi
+    print "means:", M
+    print "covariances:", C
+
+
+
+def main():
+    # assume 3 means
+    D = 2 # so we can visualize it more easily
+    s = 4 # separation so we can control how far apart the means are
+    mu1 = np.array([0, 0])
+    mu2 = np.array([s, s])
+    mu3 = np.array([0, s])
+
+    N = 2000 # number of samples
+    X = np.zeros((N, D))
+    X[:1200, :] = np.random.randn(1200, D)*2 + mu1
+    X[200:800, :] = np.random.randn(600, D) + mu2
+    X[1800:, :] = np.random.randn(200, D)*0.5 + mu3
+
+    # what does it look like without clustering?
+    plt.scatter(X[:,0], X[:,1])
+    plt.show()
+
+    K = 3
+    gmm(X, K)
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ unsupervised_class/hcluster.py
@@ -0,0 +1,41 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from scipy.cluster.hierarchy import dendrogram, linkage
+
+
+def main():
+    D = 2 # so we can visualize it more easily
+    s = 4 # separation so we can control how far apart the means are
+    mu1 = np.array([0, 0])
+    mu2 = np.array([s, s])
+    mu3 = np.array([0, s])
+
+    N = 900 # number of samples
+    X = np.zeros((N, D))
+    X[:300, :] = np.random.randn(300, D) + mu1
+    X[300:600, :] = np.random.randn(300, D) + mu2
+    X[600:, :] = np.random.randn(300, D) + mu3
+
+    Z = linkage(X, 'ward')
+    print "Z.shape:", Z.shape
+    # Z has the format [idx1, idx2, dist, sample_count]
+    # therefore, its size will be (N-1, 4)
+    plt.title("Ward")
+    dendrogram(Z)
+    plt.show()
+
+    Z = linkage(X, 'single')
+    plt.title("Single")
+    dendrogram(Z)
+    plt.show()
+
+    Z = linkage(X, 'complete')
+    plt.title("Complete")
+    dendrogram(Z)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
