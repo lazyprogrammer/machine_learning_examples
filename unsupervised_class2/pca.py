93d087f2390a8518a885b192b33f3bd0b890cde8
Mac User <sublime.balloon@gmail.com>
2017-06-18 19:16:10 -0400

    py 3

--- unsupervised_class2/pca.py
+++ unsupervised_class2/pca.py
@@ -1,5 +1,10 @@
 # https://deeplearningcourses.com/c/unsupervised-deep-learning-in-python
 # https://www.udemy.com/unsupervised-deep-learning-in-python
+from __future__ import print_function, division
+from builtins import range, input
+# Note: you may need to update your version of future
+# sudo pip install -U future
+
 import numpy as np
 import matplotlib.pyplot as plt
 
