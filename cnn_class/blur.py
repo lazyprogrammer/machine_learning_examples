8c7a31a73d6b033676c5f52a7367012ff341e1c7
Mac User <sublime.balloon@gmail.com>
2017-10-07 13:07:47 -0400

    normalize the filter

--- cnn_class/blur.py
+++ cnn_class/blur.py
@@ -28,6 +28,7 @@ for i in range(20):
     for j in range(20):
         dist = (i - 9.5)**2 + (j - 9.5)**2
         W[i, j] = np.exp(-dist / 50.)
+W /= W.sum() # normalize the kernel
 
 # let's see what the filter looks like
 plt.imshow(W, cmap='gray')
@@ -52,6 +53,7 @@ print(out.shape)
 
 # in color
 out3 = np.zeros(img.shape)
+print(out3.shape)
 for i in range(3):
     out3[:,:,i] = convolve2d(img[:,:,i], W, mode='same')
 plt.imshow(out3)
