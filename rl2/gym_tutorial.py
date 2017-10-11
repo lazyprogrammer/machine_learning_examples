a92c12dd256db9d2945991766905b71354e1718e
Mac User <sublime.balloon@gmail.com>
2017-10-11 18:03:58 -0400

    pick first action

--- rl2/gym_tutorial.py
+++ rl2/gym_tutorial.py
@@ -39,6 +39,8 @@ env.action_space
 # env.action_space.contains       env.action_space.n              env.action_space.to_jsonable
 # env.action_space.from_jsonable  env.action_space.sample
 
+# pick an action
+action = env.action_space.sample()
 
 # do an action
 observation, reward, done, info = env.step(action)
