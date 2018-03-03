21067c07e2f1950b4d5cf2b3aa8c9c32dc635ea3
Bob <bob@bob.bob>
2018-03-02 23:10:42 -0500

    add welch

--- ab_testing/ex_ttest.py
+++ ab_testing/ex_ttest.py
@@ -23,4 +23,23 @@ print("b.mean:", b.mean())
 
 # built-in t-test:
 t, p = stats.ttest_ind(a, b)
-print("t:\t", t, "p2:\t", p)
+print("t:\t", t, "p:\t", p)
+
+# welch's t-test:
+t, p = stats.ttest_ind(a, b, equal_var=False)
+print("Welch's t-test:")
+print("t:\t", t, "p:\t", p)
+
+# welch's t-test manual:
+N1 = len(a)
+s1_sq = a.var()
+N2 = len(b)
+s2_sq = b.var()
+t = (a.mean() - b.mean()) / np.sqrt(s1_sq / N1 + s2_sq / N2)
+
+nu1 = N1 - 1
+nu2 = N2 - 1
+df = (s1_sq / N1 + s2_sq / N2)**2 / ( (s1_sq*s1_sq) / (N1*N1 * nu1) + (s2_sq*s2_sq) / (N2*N2 * nu2) )
+p = (1 - stats.t.cdf(np.abs(t), df=df))*2
+print("Manual Welch t-test")
+print("t:\t", t, "p:\t", p)
