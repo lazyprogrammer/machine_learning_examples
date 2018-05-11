3f301f62313714f9dce6ba2fa819bd5899583a99
Bob <bob@bob.bob>
2018-05-11 16:29:28 -0400

    improve plot

--- unsupervised_class3/visualize_latent_space.py
+++ unsupervised_class3/visualize_latent_space.py
@@ -17,11 +17,23 @@ if __name__ == '__main__':
   # convert X to binary variable
   X = (X > 0.5).astype(np.float32)
 
+  for i in range(len(X)):
+    plt.imshow(X[i].reshape(28, 28), cmap='gray')
+    plt.title("Label: %s" % Y[i])
+    plt.show()
+    ans = input("Show another? [Y/n]")
+    if ans and ans[0].lower().startswith('n'):
+      break
+
+
   vae = VariationalAutoencoder(784, [200, 100, 2])
-  vae.fit(X)
+  vae.fit(X.copy())
+  # fit will shuffle the data
+  # so we need to copy to prevent messing up the order
+  # for plotting later, we need Z and Y to correspond
 
   Z = vae.transform(X)
-  plt.scatter(Z[:,0], Z[:,1], c=Y)
+  plt.scatter(Z[:,0], Z[:,1], c=Y, s=10)
   plt.show()
 
 
@@ -34,12 +46,12 @@ if __name__ == '__main__':
   # build Z first so we don't have to keep
   # re-calling the predict function
   # it is particularly slow in theano
-  Z = []
+  Z2 = []
   for i, x in enumerate(x_values):
     for j, y in enumerate(y_values):
       z = [x, y]
-      Z.append(z)
-  X_recon = vae.prior_predictive_with_input(Z)
+      Z2.append(z)
+  X_recon = vae.prior_predictive_with_input(Z2)
 
   k = 0
   for i, x in enumerate(x_values):
@@ -51,3 +63,4 @@ if __name__ == '__main__':
       image[(n - i - 1) * 28:(n - i) * 28, j * 28:(j + 1) * 28] = x_recon
   plt.imshow(image, cmap='gray')
   plt.show()
+
