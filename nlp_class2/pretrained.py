dc18466ecd13495fb4757c72da44b71cf2c417f7
Bob <bob@bob.bob>
2018-05-29 04:12:03 -0400

    update

---
+++ nlp_class2/pretrained.py
@@ -0,0 +1,113 @@
+# https://deeplearningcourses.com/c/data-science-natural-language-processing-in-python
+# https://www.udemy.com/data-science-natural-language-processing-in-python
+
+# Author: http://lazyprogrammer.me
+from __future__ import print_function, division
+from future.utils import iteritems
+from builtins import range
+# Note: you may need to update your version of future
+# sudo pip install -U future
+
+
+# WHERE TO GET THE VECTORS:
+# GloVe: https://nlp.stanford.edu/projects/glove/
+
+import numpy as np
+from sklearn.metrics.pairwise import pairwise_distances
+
+
+def dist1(a, b):
+    return np.linalg.norm(a - b)
+def dist2(a, b):
+    return 1 - a.dot(b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
+# pick a distance type
+dist, metric = dist2, 'cosine'
+# dist, metric = dist1, 'euclidean'
+
+
+## more intuitive
+# def find_analogies(w1, w2, w3):
+#   for w in (w1, w2, w3):
+#     if w not in word2vec:
+#       print("%s not in dictionary" % w)
+
+#   king = word2vec[w1]
+#   man = word2vec[w2]
+#   woman = word2vec[w3]
+#   v0 = king - man + woman
+
+#   min_dist = float('inf')
+#   best_word = ''
+#   for word, v1 in iteritems(word2vec):
+#     if word not in (w1, w2, w3):
+#       d = dist(v0, v1)
+#       if d < min_dist:
+#         min_dist = d
+#         best_word = word
+#   print(w1, "-", w2, "=", best_word, "-", w3)
+
+
+## faster
+def find_analogies(w1, w2, w3):
+  for w in (w1, w2, w3):
+    if w not in word2vec:
+      print("%s not in dictionary" % w)
+
+  king = word2vec[w1]
+  man = word2vec[w2]
+  woman = word2vec[w3]
+  v0 = king - man + woman
+
+  distances = pairwise_distances(v0.reshape(1, D), embedding, metric=metric).reshape(V)
+  idx = distances.argmin()
+  best_word = idx2word[idx]
+
+  print(w1, "-", w2, "=", best_word, "-", w3)
+
+
+# load in pre-trained word vectors
+print('Loading word vectors...')
+word2vec = {}
+embedding = []
+idx2word = []
+with open('../large_files/glove.6B/glove.6B.50d.txt') as f:
+  # is just a space-separated text file in the format:
+  # word vec[0] vec[1] vec[2] ...
+  for line in f:
+    values = line.split()
+    word = values[0]
+    vec = np.asarray(values[1:], dtype='float32')
+    word2vec[word] = vec
+    embedding.append(vec)
+    idx2word.append(word)
+print('Found %s word vectors.' % len(word2vec))
+embedding = np.array(embedding)
+V, D = embedding.shape
+
+
+find_analogies('king', 'man', 'woman')
+find_analogies('france', 'paris', 'london')
+find_analogies('france', 'paris', 'rome')
+find_analogies('paris', 'france', 'italy')
+find_analogies('france', 'french', 'english')
+find_analogies('japan', 'japanese', 'chinese')
+find_analogies('japan', 'japanese', 'italian')
+find_analogies('japan', 'japanese', 'australian')
+find_analogies('december', 'november', 'june')
+find_analogies('miami', 'florida', 'texas')
+find_analogies('einstein', 'scientist', 'painter')
+find_analogies('china', 'rice', 'bread')
+find_analogies('man', 'woman', 'she')
+find_analogies('man', 'woman', 'aunt')
+find_analogies('man', 'woman', 'sister')
+find_analogies('man', 'woman', 'wife')
+find_analogies('man', 'woman', 'actress')
+find_analogies('man', 'woman', 'mother')
+find_analogies('heir', 'heiress', 'princess')
+find_analogies('nephew', 'niece', 'aunt')
+find_analogies('france', 'paris', 'tokyo')
+find_analogies('france', 'paris', 'beijing')
+find_analogies('february', 'january', 'november')
+find_analogies('france', 'paris', 'rome')
+find_analogies('paris', 'france', 'italy')
