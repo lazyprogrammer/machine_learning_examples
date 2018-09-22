c06bde607e395b4c6ff23de99c738d71065b0cf2
Bob <bob@bob.bob>
2018-09-22 01:47:06 -0400

    update

---
+++ ann_class2/keras_functional.py
@@ -0,0 +1,78 @@
+# https://deeplearningcourses.com/c/data-science-deep-learning-in-theano-tensorflow
+# https://www.udemy.com/data-science-deep-learning-in-theano-tensorflow
+from __future__ import print_function, division
+from builtins import range
+# Note: you may need to update your version of future
+# sudo pip install -U future
+
+from keras.models import Model
+from keras.layers import Dense, Input
+from util import get_normalized_data, y2indicator
+
+import matplotlib.pyplot as plt
+
+# NOTE: do NOT name your file keras.py because it will conflict
+# with importing keras
+
+# installation is easy! just the usual "sudo pip(3) install keras"
+
+
+# get the data, same as Theano + Tensorflow examples
+# no need to split now, the fit() function will do it
+Xtrain, Xtest, Ytrain, Ytest = get_normalized_data()
+
+# get shapes
+N, D = Xtrain.shape
+K = len(set(Ytrain))
+
+# by default Keras wants one-hot encoded labels
+# there's another cost function we can use
+# where we can just pass in the integer labels directly
+# just like Tensorflow / Theano
+Ytrain = y2indicator(Ytrain)
+Ytest = y2indicator(Ytest)
+
+
+# ANN with layers [784] -> [500] -> [300] -> [10]
+i = Input(shape=(D,))
+x = Dense(500, activation='relu')(i)
+x = Dense(300, activation='relu')(x)
+x = Dense(K, activation='softmax')(x)
+model = Model(inputs=i, outputs=x)
+
+
+# list of losses: https://keras.io/losses/
+# list of optimizers: https://keras.io/optimizers/
+# list of metrics: https://keras.io/metrics/
+model.compile(
+  loss='categorical_crossentropy',
+  optimizer='adam',
+  metrics=['accuracy']
+)
+
+# note: multiple ways to choose a backend
+# either theano, tensorflow, or cntk
+# https://keras.io/backend/
+
+
+# gives us back a <keras.callbacks.History object at 0x112e61a90>
+r = model.fit(Xtrain, Ytrain, validation_data=(Xtest, Ytest), epochs=15, batch_size=32)
+print("Returned:", r)
+
+# print the available keys
+# should see: dict_keys(['val_loss', 'acc', 'loss', 'val_acc'])
+print(r.history.keys())
+
+# plot some data
+plt.plot(r.history['loss'], label='loss')
+plt.plot(r.history['val_loss'], label='val_loss')
+plt.legend()
+plt.show()
+
+# accuracies
+plt.plot(r.history['acc'], label='acc')
+plt.plot(r.history['val_acc'], label='val_acc')
+plt.legend()
+plt.show()
+
+
