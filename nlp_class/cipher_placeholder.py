48b5b3e97f1f80b6493025fbbc52715b77d5c6bc
Bob <bob@bob.bob>
2019-12-12 22:37:55 -0500

    update

---
+++ nlp_class/cipher_placeholder.py
@@ -0,0 +1,23 @@
+# https://deeplearningcourses.com/c/data-science-natural-language-processing-in-python
+# https://www.udemy.com/data-science-natural-language-processing-in-python
+
+# Author: http://lazyprogrammer.me
+
+# Get the data from here:
+# https://lazyprogrammer.me/course_files/moby_dick.txt
+
+### encode a message
+
+# this is a random excerpt from Project Gutenberg's
+# The Adventures of Sherlock Holmes, by Arthur Conan Doyle
+# https://www.gutenberg.org/ebooks/1661
+
+original_message = '''I then lounged down the street and found,
+as I expected, that there was a mews in a lane which runs down
+by one wall of the garden. I lent the ostlers a hand in rubbing
+down their horses, and received in exchange twopence, a glass of
+half-and-half, two fills of shag tobacco, and as much information
+as I could desire about Miss Adler, to say nothing of half a dozen
+other people in the neighbourhood in whom I was not in the least
+interested, but whose biographies I was compelled to listen to.
+'''
