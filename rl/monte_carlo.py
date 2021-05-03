886fc915902d1fc45a675afaa907036f1c05cf4f
User <user@example.com>
2021-05-03 00:04:29 -0400

    update

--- rl/monte_carlo.py
+++ rl/monte_carlo.py
@@ -10,9 +10,7 @@ import numpy as np
 from grid_world import standard_grid, negative_grid
 from iterative_policy_evaluation import print_values, print_policy
 
-SMALL_ENOUGH = 1e-5
 GAMMA = 0.9
-ALL_POSSIBLE_ACTIONS = ('U', 'D', 'L', 'R')
 
 # NOTE: this is only policy evaluation, not optimization
 
@@ -27,37 +25,34 @@ def play_game(grid, policy, max_steps=20):
   grid.set_state(start_states[start_idx])
 
   s = grid.current_state()
-  states_and_rewards = [(s, 0)] # list of tuples of (state, reward)
+
+  # keep track of all states and rewards encountered
+  states = [s]
+  rewards = [0]
+
   steps = 0
   while not grid.game_over():
     a = policy[s]
     r = grid.move(a)
-    s = grid.current_state()
-    states_and_rewards.append((s, r))
+    next_s = grid.current_state()
+
+    # update states and rewards lists
+    states.append(next_s)
+    rewards.append(r)
 
     steps += 1
     if steps >= max_steps:
       break
 
-  # calculate the returns by working backwards from the terminal state
+    # update state
+    # note: there is no need to store the final terminal state
+    s = next_s
 
   # we want to return:
-  # states  = [s(0), s(1), ..., s(T-1)]
-  # returns = [G(0), G(1), ..., G(T-1)]
-  G = 0
-  states_and_returns = []
-  first = True
-  for s, r in reversed(states_and_rewards):
-    # the value of the terminal state is 0 by definition
-    # we should ignore the first state we encounter
-    # and ignore the last G, which is meaningless since it doesn't correspond to any move
-    if first:
-      first = False
-    else:
-      states_and_returns.append((s, G))
-    G = r + GAMMA*G
-  states_and_returns.reverse() # we want it to be in order of state visited
-  return states_and_returns
+  # states  = [s(0), s(1), ..., S(T)]
+  # rewards = [R(0), R(1), ..., R(T)]
+
+  return states, rewards
 
 
 if __name__ == '__main__':
@@ -95,17 +90,19 @@ if __name__ == '__main__':
 
   # repeat
   for t in range(100):
-
     # generate an episode using pi
-    states_and_returns = play_game(grid, policy)
-    seen_states = set()
-    for s, G in states_and_returns:
-      # check if we have already seen s
-      # called "first-visit" MC policy evaluation
-      if s not in seen_states:
+    states, rewards = play_game(grid, policy)
+    G = 0
+    T = len(states)
+    for t in range(T - 2, -1, -1):
+      s = states[t]
+      r = rewards[t+1]
+      G = r + GAMMA * G # update return
+
+      # we'll use first-visit Monte Carlo
+      if s not in states[:t]:
         returns[s].append(G)
         V[s] = np.mean(returns[s])
-        seen_states.add(s)
 
   print("values:")
   print_values(V, grid)
